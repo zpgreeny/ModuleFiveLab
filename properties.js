d087297f220fafa5ff067323a56d03e2851e0ad4
zach <zpgreen@lakeheadu.ca>
2020-04-02 19:32:40 -0400

    4

--- properties.js
+++ properties.js
@@ -20,7 +20,7 @@ function Props(jsonObj) {
     for (let i = 0; i < Props.length; i++) {
 
         let article = document.createElement('article');
-        let section = document.querySelector('section');
+        let main = document.querySelector('main');
         let list = document.createElement('p');
         let p1 = document.createElement('p');
         let p2 = document.createElement('p');
@@ -112,7 +112,6 @@ function Props(jsonObj) {
 
         getSuperRating();
 
-
         let features = Props[i].features;
         //for loop to store each index array into a list
         for (let j = 0; j < features.length[j]; j++) {
@@ -128,32 +127,355 @@ function Props(jsonObj) {
         });
 
         function description1() {
+
             article.appendChild(p1);
             article.appendChild(p2);
             article.appendChild(p3);
             article.appendChild(p4);
             article.appendChild(p5);
-
             article.appendChild(p7);
             article.appendChild(p8);
             article.appendChild(list);
-            section.appendChild(article);
+            main.appendChild(article);
         }
 
         let availabilityPropOne = document.querySelectorAll('button')[1];
 
         availabilityPropOne.addEventListener('click', function() {
-            availability1()
-        })
+            availability1();
+        });
 
         function availability1() {
+
             article.appendChild(p6);
-            section.appendChild(article);
+            main.appendChild(article);
         }
 
 
     }
 }
+
+
+
+let URL2 = "https://zpgreeny.github.io/ModuleFiveLab/properties2.json";
+
+let request2 =new XMLHttpRequest();
+request2.open('GET',URL2);
+request2.responseType ='json';
+request2.send();
+
+request2.onload = function() {
+    let Prop2 = request2.response;
+    console.log(Prop2);
+    Props2(Prop2);
+}
+
+
+function Props2(jsonObj) {
+
+    let Props2 = jsonObj.properties2;
+
+    for (let i = 0; i < Props2.length; i++) {
+
+        let article2 = document.createElement('article');
+        let main2 = document.querySelector('main');
+        let list2 = document.createElement('p');
+        let p12 = document.createElement('p');
+        let p22 = document.createElement('p');
+        let p32 = document.createElement('p');
+        let p42 = document.createElement('p');
+        let p52 = document.createElement('p');
+        let p62 = document.createElement('p');
+        let p72 = document.createElement('p');
+        let p82 = document.createElement('p');
+
+
+        async function getName2() {
+            try {
+                let names2 = await Promise.all(p12.textContent = Props2[i].name);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getName2();
+
+        async function getPrice2() {
+            try {
+                let prices2 = await Promise.all(p22.textContent = 'Price: ' + Props2[i].price);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getPrice2();
+
+        async function getRating2() {
+            try {
+                let ratings2 = await Promise.all(p32.textContent = 'Rating: ' + Props2[i].rating);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getRating2();
+
+        async function getLocation2() {
+            try {
+                let locations2 = await Promise.all(p42.textContent = Props2[i].location);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getLocation2();
+
+        async function getRooms2() {
+            try {
+                let rooms2 = await Promise.all(p52.textContent = 'Rooms: ' + Props2[i].rooms);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getRooms2();
+
+        async function getAvailability2() {
+            try {
+                let availabilities2 = await Promise.all(p62.textContent = Props2[i].availability);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getAvailability2();
+
+        async function getFeatures2() {
+            try {
+                let features2 = await Promise.all(p72.textContent = 'Features: ' + Props2[i].features);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getFeatures2();
+
+        async function getSuperRating2() {
+            try {
+                let superRating2 = await Promise.all(p82.textContent = Props2[i].superRating);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getSuperRating2();
+
+        let features = Props2[i].features;
+        //for loop to store each index array into a list
+        for (let j = 0; j < features.length[j]; j++) {
+            let listItem2 = document.createElement('li');
+            listItem2.textContent = features[j];
+            ul.appendChild(listitem2);
+        }
+
+        let descriptionPropTwo = document.querySelectorAll('button')[2];
+
+        descriptionPropTwo.addEventListener('click', function () {
+            description2();
+        });
+
+        function description2() {
+
+            article2.appendChild(p12);
+            article2.appendChild(p22);
+            article2.appendChild(p32);
+            article2.appendChild(p42);
+            article2.appendChild(p52);
+            article2.appendChild(p72);
+            article2.appendChild(p82);
+            article2.appendChild(list2);
+            main2.appendChild(article2);
+        }
+
+        let availabilityPropTwo = document.querySelectorAll('button')[3];
+
+        availabilityPropTwo.addEventListener('click', function() {
+            availability2();
+        });
+
+        function availability2() {
+
+            article2.appendChild(p62);
+            main2.appendChild(article2);
+        }
+
+
+    }
+}
+
+
+
+
+let URL3 = "https://zpgreeny.github.io/ModuleFiveLab/properties3.json";
+
+let request3 =new XMLHttpRequest();
+request3.open('GET',URL3);
+request3.responseType ='json';
+request3.send();
+
+request3.onload = function() {
+    let Prop3 = request3.response;
+    console.log(Prop3);
+    Props3(Prop3);
+}
+
+
+function Props3(jsonObj) {
+
+    let Props3 = jsonObj.properties3;
+
+    for (let i = 0; i < Props3.length; i++) {
+
+        let article3 = document.createElement('article');
+        let main3 = document.querySelector('main');
+        let list3 = document.createElement('p');
+        let p13 = document.createElement('p');
+        let p23 = document.createElement('p');
+        let p33 = document.createElement('p');
+        let p43 = document.createElement('p');
+        let p53 = document.createElement('p');
+        let p63 = document.createElement('p');
+        let p73 = document.createElement('p');
+        let p83 = document.createElement('p');
+
+
+        async function getName3() {
+            try {
+                let names3 = await Promise.all(p13.textContent = Props3[i].name);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getName3();
+
+        async function getPrice3() {
+            try {
+                let prices3 = await Promise.all(p23.textContent = 'Price: ' + Props3[i].price);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getPrice3();
+
+        async function getRating3() {
+            try {
+                let ratings3 = await Promise.all(p33.textContent = 'Rating: ' + Props3[i].rating);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getRating3();
+
+        async function getLocation3() {
+            try {
+                let locations3 = await Promise.all(p43.textContent = Props3[i].location);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getLocation3();
+
+        async function getRooms3() {
+            try {
+                let rooms3 = await Promise.all(p53.textContent = 'Rooms: ' + Props3[i].rooms);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getRooms3();
+
+        async function getAvailability3() {
+            try {
+                let availabilities3 = await Promise.all(p63.textContent = Props3[i].availability);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getAvailability3();
+
+        async function getFeatures3() {
+            try {
+                let features3 = await Promise.all(p73.textContent = 'Features: ' + Props3[i].features);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getFeatures3();
+
+        async function getSuperRating3() {
+            try {
+                let superRating3 = await Promise.all(p83.textContent = Props3[i].superRating);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        getSuperRating3();
+
+        let features = Props3[i].features;
+        //for loop to store each index array into a list
+        for (let j = 0; j < features.length[j]; j++) {
+            let listItem3 = document.createElement('li');
+            listItem3.textContent = features[j];
+            ul.appendChild(listitem3);
+        }
+
+        let descriptionPropThree = document.querySelectorAll('button')[4];
+
+        descriptionPropThree.addEventListener('click', function () {
+            description3();
+        });
+
+        function description3() {
+
+            article3.appendChild(p13);
+            article3.appendChild(p23);
+            article3.appendChild(p33);
+            article3.appendChild(p43);
+            article3.appendChild(p53);
+            article3.appendChild(p73);
+            article3.appendChild(p83);
+            article3.appendChild(list3);
+            main3.appendChild(article3);
+        }
+
+        let availabilityPropThree = document.querySelectorAll('button')[6];
+
+        availabilityPropThree.addEventListener('click', function() {
+            availability3();
+        });
+
+        function availability3() {
+
+            article3.appendChild(p63);
+            main3.appendChild(article3);
+        }
+
+
+    }
+}
+
+
+
+
+
 
 
 
